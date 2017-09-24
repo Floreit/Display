5d6e598f17169e92c1ce59d2f6386f8646977d2e
Floreit <Floreit@users.noreply.github.com>
2017-09-24 00:36:36 -0500

    Add files via upload

--- KyleLud4.3.py
+++ KyleLud4.3.py
@@ -7,31 +7,29 @@ totalRain = 0
 while yearsCount < years:
     monthsCount = 0
 
-
     EndMonthRain = 0
     monthsCount = 0
-    print("totalRainYears = ", totalRainYears)
-    print("yearcount = ", yearsCount)
+    # print("totalRainYears = ", totalRainYears)
+    # print("yearcount = ", yearsCount)
     while monthsCount < 12:
         rain = input("Please enter in the number of inches of rainfall this month")
         rain = int(rain)
         monthsCount = monthsCount + 1
         totalRain = totalRain + rain
-        print("totalrain = ", totalRain , "monthsCount = ", monthsCount)
+        print("totalrain = ", totalRain, "monthsCount = ", monthsCount)
         if monthsCount == 12:
             EndMonthRain = totalRain
             totalRainYears = EndMonthRain + totalRainYears
             yearsCount = yearsCount + 1
-            print(" placeholder ")
+            print(" End of Year ")
         elif yearsCount == years:
             print("you fucked up you idiot")
 
-
 N = yearsCount * 12
 average = totalRainYears / N
-round(average,2)
+average = round(average,2)
 
 # print("Yearcount = ", yearsCount) # Testing purposes
 print("Total number of months = ", N)
 print("TotalRainfall = ", totalRainYears)
-print("The Average Rainfall Per Month",average)
+print("The Average Rainfall Per Month", average)
